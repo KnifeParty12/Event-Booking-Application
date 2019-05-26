a2f57c032d7054c33be06784dde02da7ea02f087
KnifeParty12 <flashfunk01@gmail.com>
2019-05-26 18:13:48 +0530

    Added types & Data to the Api
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- app.js
+++ app.js
@@ -5,14 +5,34 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
+
 app.use('/graphql',graphqlHttp({
         schema: buildSchema(`
+        
+           type Event {
+             _id: ID!
+             title: String!
+             description: String!
+             price: Float!
+             date: String!   
+           } 
+            
+           input EventInput {
+             title: String!
+             description: String!
+             price: Float!
+             date: String!
+           } 
+            
+            
            type RootQuery {
-                events: [String!]!
+                events: [Event!]!
            }
            
            type RootMutation{
-                createEvent(name: String): String    
+                createEvent(eventInput: EventInput ): Event    
            }
            
             schema{
@@ -20,13 +40,20 @@ app.use('/graphql',graphqlHttp({
                 mutation: RootMutation
             }
         `),
-        rootValue: {
+        rootValue: {                 /*Resolver Functions*/
             events: () => {
-                return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+                return events;
             },
-            createEvent: (args) => {
-                    const eventName = args.name;
-                    return eventName;
+            createEvent: args => {
+             const event = {
+                 _id: Math.random().toString(),
+                 title: args.eventInput.title,
+                 description: args.eventInput.description,
+                 price: +args.eventInput.price,
+                 date: args.eventInput.date
+             };
+             events.push(event);
+             return event;
             }
         },
             graphiql: true
